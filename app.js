3a8834eece5207be8ec3535b5dbc8cfb5346fba3
Moon Patel <moonpatel2003@gmail.com>
2022-08-14 08:43:18 +0530

    added data seeder

--- app.js
+++ app.js
@@ -8,35 +8,74 @@ const Campground = require('./models/campgrounds')
 const port = 3000
 
 // connect to MongoDB database
-mongoose.connect('mongodb://localhost:27017/yelpcamp')
+mongoose.connect('mongodb://localhost:27017/yelpcamp', {
+})
 
 // check the status of database connection
-const db = mongoose.connection
-db.on("error", console.error.bind(console,"connection error"))
-db.once("open", () => {
-    console.log("Database connected")
+// const db = mongoose.connection
+// db.on("error", console.error.bind(console,"connection error"))
+// db.once("open", () => {
+//     console.log("Database connected")
+// })
+
+
+
+
+
+const cities = require('./seeds/cities')
+const {places,descriptors} = require('./seeds/seedHelpers')
+
+const sample = data => data[Math.floor(Math.random()*data.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({})     // delete all the data first
+
+    for (let i = 0; i < 50; i++) {
+        const city = sample(cities)
+        const desc = `${sample(descriptors)} ${sample(places)}}`
+        const cg = new Campground({
+            title: desc,
+            location: `${city.city, city.state}`
+        })
+        console.log(desc,`${city.city, city.state}`)
+        await cg.save()
+    }
+}
+
+seedDB()
+.then(() => {
+    console.log('Data added')
+    mongoose.connection.close();
+})
+.catch((err) => {
+    console.log('Error in seeding data',err)
 })
 
-// create an express application object
-const app = express()
 
-// set parameters for rendering templates
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'views/'))
 
-// RECEIVE REQUESTS
-// main page
-app.get('/',(req,res) => {
-    res.render('home')
-})
 
-// campgrounds
-app.get('/campgrounds',(req,res) => {
-    const camp = new Campground({title:'Backyard'})
-    res.send(camp)
-})
 
-// listen for incoming requests
-app.listen(port, () => {
-    console.log(`LISTENING ON PORT ${port}`)
-})
+
+// // create an express application object
+// const app = express()
+
+// // set parameters for rendering templates
+// app.set('view engine','ejs')
+// app.set('views',path.join(__dirname,'views/'))
+
+// // RECEIVE REQUESTS
+// // main page
+// app.get('/',(req,res) => {
+//     res.render('home')
+// })
+
+// // campgrounds
+// app.get('/campgrounds',(req,res) => {
+//     const camp = new Campground({title:'Backyard'})
+//     res.send(camp)
+// })
+
+// // listen for incoming requests
+// app.listen(port, () => {
+//     console.log(`LISTENING ON PORT ${port}`)
+// })
---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+// including required packages
+const mongoose = require('mongoose')
+
+// require models
+const Campground = require('../models/campgrounds')
+
+// connect to MongoDB database
+mongoose.connect('mongodb://localhost:27017/yelpcamp')
+
+// check the status of database connection
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error"))
+db.once("open", () => console.log("Database connected"))
+
+const cities = require('./cities')
+const {places,descriptors} = require('./seedHelpers')
+
+const sample = data => data[Math.floor(Math.random()*data.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({})     // delete all the data first
+
+    for (let i = 0; i < 50; i++) {
+        const randomNum = Math.floor(Math.random() *1000)
+        // create an instance of a campground
+        const cg = new Campground({
+            location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+            title: `${sample(descriptors)} ${sample(places)}}`
+        })
+        // save it to database
+        await cg.save()
+    }
+}
+
+seedDB()
+.then(() => mongoose.connection.close())
+.catch(err => console.log('Error in seeding data',err))
